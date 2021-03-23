4213eab1f6d806778b4c56330ed1a2ec7086005c
Alexander Ulbrich <alexander.ulbrich@uni-ulm.de>
2021-03-23 17:04:42 +0100

    added temperatur dht22 node

--- nodes/temperature_node/firmware/temperature_node/temperature_node.ino
+++ nodes/temperature_node/firmware/temperature_node/temperature_node.ino
@@ -1,23 +1,25 @@
 #include <SPI.h>
 #include <NRFLite.h>
+#include "DHT.h"     
+#include "LowPower.h" 
+     
+#define DHTPIN 8         
+#define DHTTYPE DHT22 
 
-#define MAX_DATA 32
-
+DHT dht(DHTPIN, DHTTYPE);
 NRFLite radio;
 
+#define MAX_DATA 32
 uint8_t data[MAX_DATA];
-
 #define TOPIC_SIZE 8
 const char* topic = "plt/temp";
-#define CONTENT_SIZE 2
-const char* content = "10";
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("start");
-    Serial.println(radio.init(1, 6, 7)); // Set radio to Id = 0, along with its CE and CSN pins
-    Serial.println("init");
+    pinMode(5,OUTPUT);
+    digitalWrite(5,LOW);
+    dht.begin();
+    delay(2000);  
 }
 
 uint8_t calc_checksum(uint8_t* data, int data_length)
@@ -30,21 +32,33 @@ uint8_t calc_checksum(uint8_t* data, int data_length)
 
 void loop()
 {
+    radio.init(1, 9, 10);
+    //measure
+    float h = dht.readHumidity();    // Lesen der Luftfeuchtigkeit und speichern in die Variable h
+    float t = dht.readTemperature(); // Lesen der Temperatur in °C und speichern in die Variable t
+    if (isnan(h) || isnan(t)) {       
+      Serial.println("Fehler beim auslesen des Sensors!");
+      delay(1000);
+      return;
+    }
+    
+    //prepare package
     int topic_size = TOPIC_SIZE;
-    int content_size = CONTENT_SIZE;
     for(int i = 0; i < topic_size; i++)
       data[i] = topic[i];
     data[topic_size] = '\n';
+    char content[20];
+    int content_size = sprintf(content, "%d,%d", int(t*10), int(h*10));
     for(int i = 0; i < content_size; i++)
-      data[TOPIC_SIZE+1+i] = content[i];
-    int length = TOPIC_SIZE+1+CONTENT_SIZE;
+      data[topic_size+1+i] = content[i];
+    int length = topic_size+1+content_size;
     uint8_t checksum = calc_checksum(data,length);
     data[length] = checksum;
     length++;
-    Serial.println("send");
-    Serial.println(topic);
-    Serial.println(content);
-    Serial.println(checksum);
     radio.send(0, data, length);
-    delay(1000);
+    digitalWrite(5,HIGH);
+    radio.powerDown();
+    for(int i = 0; i < 60; i++)
+      LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+    digitalWrite(5,LOW);
 }
